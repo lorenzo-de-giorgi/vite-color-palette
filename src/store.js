c9dbb2ebfbc171be3a3ae30f335a541fa3a15f98
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-04-24 12:20:40 +0200

    added store.js

---
+++ src/store.js
@@ -0,0 +1,91 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    colors: [
+        {
+          label: "rich-black",
+          hex: "#03071eff",
+          palette_name: "fire",
+        },
+        {
+          label: "chocolate-cosmos",
+          hex: "#370617ff",
+          palette_name: "fire",
+        },
+        {
+          label: "rosewood",
+          hex: "#6a040fff",
+          palette_name: "fire",
+        },
+        {
+          label: "penn-red",
+          hex: "#9d0208ff",
+          palette_name: "fire",
+        },
+        {
+          label: "engineering-orange",
+          hex: "#d00000ff",
+          palette_name: "fire",
+        },
+        {
+          label: "sinopia",
+          hex: "#dc2f02ff",
+          palette_name: "fire",
+        },
+        {
+          label: "persimmon",
+          hex: "#e85d04ff",
+          palette_name: "fire",
+        },
+        {
+          label: "princeton-orange",
+          hex: "#f48c06ff",
+          palette_name: "fire",
+        },
+        {
+          label: "orange-web",
+          hex: "#faa307ff",
+          palette_name: "fire",
+        },
+        {
+          label: "selective-yellow",
+          hex: "#ffba08ff",
+          palette_name: "fire",
+        },
+        {
+          label: "imperial-red",
+          hex: "#f94144ff",
+          palette_name: "summer",
+        },
+        {
+          label: "orange-crayola",
+          hex: "#f3722cff",
+          palette_name: "summer",
+        },
+        {
+          label: "carrot-orange",
+          hex: "#f8961eff",
+          palette_name: "summer",
+        },
+        {
+          label: "saffron",
+          hex: "#f9c74fff",
+          palette_name: "summer",
+        },
+        {
+          label: "pistachio",
+          hex: "#90be6dff",
+          palette_name: "summer",
+        },
+        {
+          label: "zomp",
+          hex: "#43aa8bff",
+          palette_name: "summer",
+        },
+        {
+          label: "paynes-gray",
+          hex: "#577590ff",
+          palette_name: "summer",
+        },
+    ]
+})
